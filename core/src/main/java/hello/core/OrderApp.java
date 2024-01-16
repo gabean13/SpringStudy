4bdb29df5d298fb709965529d8a6cf58eaca149d
gabean13 <gazette9@naver.com>
2024-01-16 22:00:09 +0900

    2 [섹션 2] - 스프링 핵심 원리 이해 2

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -7,14 +7,15 @@ import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member1 = new Member(memberId, "gabin", Grade.VIP);
         memberService.join(member1);
 
-        Order order = orderService.createOrder(memberId, "book", 10000);
+        Order order = orderService.createOrder(memberId, "book", 15000);
         System.out.println(order);
 
     }
---
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,41 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10%할인이 적용되어야 함")
+    void vip_o(){
+        //given
+        Member member1 = new Member(1L, "member1", Grade.VIP);
+
+        //when
+        int discount = rateDiscountPolicy.discount(member1, 10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("BASIC은 할인이 0원이어야함")
+    void vip_x(){
+        //given
+        Member member2 = new Member(2L, "member2", Grade.BASIC);
+
+        //when
+        int discount = rateDiscountPolicy.discount(member2, 10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+
+}
