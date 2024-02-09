a711176cc81850edaf39061edb4553ee8316e8ca
gabean13 <gazette9@naver.com>
2024-02-10 01:06:12 +0900

    3 [섹션 4] - front Controller V2 view분리

--- servlet/src/main/java/hello/servlet/web/servletMvc/MvcMemberSaveServlet.java
+++ servlet/src/main/java/hello/servlet/web/servletMvc/MvcMemberSaveServlet.java
@@ -10,7 +10,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
 public class MvcMemberSaveServlet extends HttpServlet {
