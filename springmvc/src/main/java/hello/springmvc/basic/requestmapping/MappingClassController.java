4e1706dab9d7b905117c6de09a0e11de11ae9fde
gabean13 <gazette9@naver.com>
2024-02-12 15:02:16 +0900

    3 [섹션 6] - HTTPMapping practice

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -1,4 +1,40 @@
 package hello.springmvc.basic.requestmapping;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
 public class MappingClassController {
+    /**
+     * 회원 목록 조회: GET /mapping/users
+     * 회원 등록: POST /mapping/users
+     * 회원 조회: GET /mapping/users/id1
+     * 회원 수정: PATCH /mapping/users/id1
+     * 회원 삭제: DELETE /mapping/users/id1
+     */
+
+    @GetMapping
+    public String getUsers() {
+        return "get users";
+    }
+
+    @PostMapping()
+    public String addUser() {
+        return "add user";
+    }
+
+    @GetMapping("/{userid}")
+    public String getUser(@PathVariable String userid) {
+        return "get user = " + userid;
+    }
+
+    @PatchMapping("/{userid}")
+    public String patchUser(@PathVariable String userid) {
+        return "fix user = " + userid;
+    }
+
+    @DeleteMapping("/{userid}")
+    public String deleteUser(@PathVariable String userid) {
+        return "delete user = " + userid;
+    }
 }
