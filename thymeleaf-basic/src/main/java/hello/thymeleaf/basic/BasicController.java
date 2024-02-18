825a5d85fd792818e80205e533be2e80f2a1f4d5
gabean13 <gazette9@naver.com>
2024-02-18 18:32:56 +0900

    4 [섹션 1] - 타임리프

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -61,8 +62,46 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/date")
+    public String basicDate(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("/link")
+    public String basicLink(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+    @GetMapping("/literal")
+    public String basicLiteral(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String basicOperation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring");
+        return "basic/operation";
+    }
+
+    @GetMapping("/attribute")
+    public String basicAttribute() {
+        return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String basicEach(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+
     @Component("helloBean")
-    static class HelloBean{
+    static class HelloBean {
         public String hello(String data) {
             return "Hello " + data;
         }
@@ -78,4 +117,18 @@ public class BasicController {
             this.age = age;
         }
     }
+
+    private Model addUsers(Model model) {
+        user userA = new user("userA", 10);
+        user userB = new user("userB", 20);
+        user userC = new user("userC", 30);
+        List<user> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+        list.add(userC);
+
+        model.addAttribute("users", list);
+        return model;
+    }
+
 }
